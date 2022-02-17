52ae589ee30edd4d36d83f09fb017836ebe6b68e
amandaelfassy1 <61420264+amandaelfassy1@users.noreply.github.com>
2022-02-17 18:22:14 +0100

    first commit

--- src/page/Post.screen.js
+++ src/page/Post.screen.js
@@ -1,93 +1,105 @@
-import * as React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  Dimensions,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+} from 'react-native';
 import {Icon} from 'react-native-elements';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Login from '../auth/Login';
-import Register from '../auth/Register.screen';
-import Post from '../page/Post.screen';
-import Home from '../page/Home';
-import Search from '../page/Search';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createStackNavigator} from '@react-navigation/stack';
 
-const Tab = createBottomTabNavigator();
+const {width, height} = Dimensions.get('window');
 
-const AuthStack = createNativeStackNavigator();
-const PostScreen = createStackNavigator();
-const SearchScreen = createStackNavigator();
-
-function AuthStackScreen() {
+function Home() {
+  const navigation = useNavigation();
   return (
-    <AuthStack.Navigator>
-      <AuthStack.Screen name="Login" component={Login} />
-      <AuthStack.Screen name="Register" component={Register} />
-    </AuthStack.Navigator>
-  );
-}
-
-function PostStackScreen() {
-  return (
-    <PostScreen.Navigator>
-      <PostScreen.Screen name="Home" component={Home} />
-      <PostScreen.Screen name="Post" component={Post} />
-    </PostScreen.Navigator>
-  );
-}
-
-function SearchStackScreen() {
-  return (
-    <SearchScreen.Navigator>
-      <SearchScreen.Screen name="Search" component={Search} />
-    </SearchScreen.Navigator>
-  );
-}
-
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
-          let iconName;
-
-          if (route.name === 'Auth') {
-            iconName = focused ? 'ios-person' : 'person-outline';
-          } else if (route.name === 'Feed') {
-            iconName = focused ? 'ios-home' : 'home-outline';
-          } else if (route.name === 'Search') {
-            iconName = focused ? 'search' : 'search';
-          }
-
-          // You can return any component that you like here!
-          return (
-            <Icon name={iconName} size={size} type="ionicon" color="#FF0000" />
-          );
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}>
-      <Tab.Screen
-        name="Feed"
-        component={PostStackScreen}
-        options={{headerShown: false}}
+    <View style={styles.content}>
+      <Image
+        onPress={() => navigation.push('Post')}
+        style={styles.imagePost}
+        source={require('../assets/img/post.png')}
       />
-      <Tab.Screen
-        name="Search"
-        component={SearchStackScreen}
-        options={{headerShown: false}}
-      />
-      <Tab.Screen
-        name="Auth"
-        component={AuthStackScreen}
-        options={{headerShown: false}}
-      />
-    </Tab.Navigator>
+      <View style={styles.contentBottom}>
+        <Text style={styles.textPost}>
+          Le Lorem Ipsum est simplement du faux texte employé dans la
+          composition et la mise en page avant impression. Le Lorem Ipsum est le
+          faux texte standard de l'imprimerie depuis les années 1500, quand un
+          imprimeur anonyme assembla ensemble des morceaux de texte pour
+          réaliser un livre spécimen de polices de texte.
+        </Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Envoyer un message</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonBuy}>
+          <Text style={styles.textButton}>Acheter</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
-}
+const styles = StyleSheet.create({
+  textPost: {
+    bottom: 0,
+    textAlign: 'center',
+    padding: 30,
+    top: 30,
+    marginBottom: 30,
+  },
+  contentBottom: {
+    border: 100,
+    bottom: 100,
+    height: '100%',
+    borderRadius: 100,
+    alignItems: 'center',
+    backgroundColor: '#EBF5FF',
+  },
+  imagePost: {
+    height: 430,
+    width: '100%',
+  },
+  contentFlex: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  imageProfil: {
+    marginRight: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+
+  buttonBuy: {
+    backgroundColor: '#4671FF',
+    width: '80%',
+    borderRadius: 10,
+    height: 50,
+    alignItems: 'center',
+    overflow: 'hidden',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  button: {
+    width: '80%',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '4671FF',
+    height: 50,
+    alignItems: 'center',
+    overflow: 'hidden',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+});
+
+export default Home;
--- src/page/Search.js
+++ src/page/Search.js
@@ -1,151 +1,151 @@
-// import React, {Component} from 'react';
-// import {
-//   Image,
-//   Text,
-//   View,
-//   Dimensions,
-//   ScrollView,
-//   RefreshControl,
-//   Alert,
-// } from 'react-native';
-// const {width} = Dimensions.get('window');
-// const columnWidth = (width - 10) / 2 - 10;
-// import Masonry from 'react-native-masonry-layout';
-// import {SafeAreaView} from 'react-native-safe-area-context';
+import React, {Component} from 'react';
+import {
+  Image,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+  RefreshControl,
+  Alert,
+} from 'react-native';
+const {width} = Dimensions.get('window');
+const columnWidth = (width - 10) / 2 - 10;
+import Masonry from 'react-native-masonry-layout';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-// export class Search extends Component {
-//   componentDidMount() {
-//     this.load();
-//   }
+export class Search extends Component {
+  componentDidMount() {
+    this.load();
+  }
 
-//   load() {
-//     let data = [
-//       {
-//         image:
-//           'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
-//         text: 'Test',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://www.adobe.com/content/dam/cc/us/en/products/creativecloud/stock/stock-riverflow1-720x522.jpg.img.jpg',
-//         text: 'Image1',
-//         key: 1,
-//       },
-//       {
-//         image: 'https://cdn.eso.org/images/thumb300y/eso1907a.jpg',
-//         text: 'Image1',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=',
-//         text: 'Image3',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg',
-//         text: 'Image1',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=',
-//         text: 'Image3',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg',
-//         text: 'Image1',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=',
-//         text: 'Image3',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg',
-//         text: 'Image1',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=',
-//         text: 'Image3',
-//         key: 1,
-//       },
-//       {
-//         image:
-//           'https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg',
-//         text: 'Image1',
-//         key: 1,
-//       },
-//     ];
+  load() {
+    let data = [
+      {
+        image:
+          'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+        text: 'Test',
+        key: 1,
+      },
+      {
+        image:
+          'https://www.adobe.com/content/dam/cc/us/en/products/creativecloud/stock/stock-riverflow1-720x522.jpg.img.jpg',
+        text: 'Image1',
+        key: 1,
+      },
+      {
+        image: 'https://cdn.eso.org/images/thumb300y/eso1907a.jpg',
+        text: 'Image1',
+        key: 1,
+      },
+      {
+        image:
+          'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=',
+        text: 'Image3',
+        key: 1,
+      },
+      {
+        image:
+          'https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg',
+        text: 'Image1',
+        key: 1,
+      },
+      {
+        image:
+          'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=',
+        text: 'Image3',
+        key: 1,
+      },
+      {
+        image:
+          'https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg',
+        text: 'Image1',
+        key: 1,
+      },
+      {
+        image:
+          'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=',
+        text: 'Image3',
+        key: 1,
+      },
+      {
+        image:
+          'https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg',
+        text: 'Image1',
+        key: 1,
+      },
+      {
+        image:
+          'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=',
+        text: 'Image3',
+        key: 1,
+      },
+      {
+        image:
+          'https://i.pinimg.com/originals/46/da/e5/46dae512e375bee2664a025507da8795.jpg',
+        text: 'Image1',
+        key: 1,
+      },
+    ];
 
-//     let last = data.length;
-//     let new_data = [];
-//     data.map((item, index) => {
-//       let height = 100;
-//       if (index === 0) {
-//         console.log(index, ':', 150);
-//       } else if (index === last - 1 && last % 2 == 0) {
-//         console.log(index, ':', 150);
-//       } else {
-//         console.log(index, ':', 250);
-//         height = 200;
-//       }
+    let last = data.length;
+    let new_data = [];
+    data.map((item, index) => {
+      let height = 100;
+      if (index === 0) {
+        console.log(index, ':', 150);
+      } else if (index === last - 1 && last % 2 == 0) {
+        console.log(index, ':', 150);
+      } else {
+        console.log(index, ':', 250);
+        height = 200;
+      }
 
-//       new_data.push({
-//         image: item.image,
-//         height: height,
-//         text: item.text,
-//       });
+      new_data.push({
+        image: item.image,
+        height: height,
+        text: item.text,
+      });
 
-//       if (index === last - 1 && last % 2 !== 0) {
-//         new_data.push({
-//           image:
-//             'https://i.pinimg.com/originals/c9/22/68/c92268d92cf2dbf96e3195683d9e14fb.png',
-//           height: 100,
-//           text: 'No data ',
-//         });
-//       }
-//     });
+      if (index === last - 1 && last % 2 !== 0) {
+        new_data.push({
+          image:
+            'https://i.pinimg.com/originals/c9/22/68/c92268d92cf2dbf96e3195683d9e14fb.png',
+          height: 100,
+          text: 'No data ',
+        });
+      }
+    });
 
-//     this.refs.list.addItems(new_data);
-//   }
+    this.refs.list.addItems(new_data);
+  }
 
-//   render() {
-//     return (
-//       <SafeAreaView style={{display: 'flex', flex: 1, backgroundColor: '#EEE'}}>
-//         <Masonry
-//           style={{flex: 1}}
-//           columns={2}
-//           ref="list"
-//           containerStyle={{padding: 5}}
-//           renderItem={item => (
-//             <View
-//               style={{
-//                 margin: 5,
-//                 backgroundColor: '#fff',
-//                 borderRadius: 5,
-//                 overflow: 'hidden',
-//                 borderWidth: 1,
-//                 borderColor: '#dedede',
-//               }}>
-//               <Image source={{uri: item.image}} style={{height: item.height}} />
+  render() {
+    return (
+      <SafeAreaView style={{display: 'flex', flex: 1, backgroundColor: '#EEE'}}>
+        <Masonry
+          style={{flex: 1}}
+          columns={2}
+          ref="list"
+          containerStyle={{padding: 5}}
+          renderItem={item => (
+            <View
+              style={{
+                margin: 5,
+                backgroundColor: '#fff',
+                borderRadius: 5,
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '#dedede',
+              }}>
+              <Image source={{uri: item.image}} style={{height: item.height}} />
 
-//               <Text style={{padding: 5, color: '#444'}}>{item.text}</Text>
-//             </View>
-//           )}
-//         />
-//       </SafeAreaView>
-//     );
-//   }
-// }
+              <Text style={{padding: 5, color: '#444'}}>{item.text}</Text>
+            </View>
+          )}
+        />
+      </SafeAreaView>
+    );
+  }
+}
 
-// export default Search;
+export default Search;
